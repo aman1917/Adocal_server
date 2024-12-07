f96117aef31928256c9aaa84727f76353a9403d5
Aman Gupta <138776305+aman1917@users.noreply.github.com>
2024-12-07 15:17:29 +0530

    Update index.js
    New Port updated as requirement to render deployement

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
-
+const PORT = process.env.PORT||8000;
 const mongoURI = process.env.MONGO_URI;
 
 // Correct CORS setup
